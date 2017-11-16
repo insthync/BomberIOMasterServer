d99d0bb081587d3d9088f7635afa0a60061743d4
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-11-17 02:30:59 +0700

    Fix unsaved character appearance

--- Scripts/UI/UIMainMenuWithMsf.cs
+++ Scripts/UI/UIMainMenuWithMsf.cs
@@ -10,12 +10,18 @@ public class UIMainMenuWithMsf : UIMainMenu
 
     public void OnClickRoomList()
     {
+        PlayerSave.SetCharacter(SelectCharacter);
+        PlayerSave.SetHead(SelectHead);
+        PlayerSave.SetBomb(SelectBomb);
         if (networkRoomDialog != null)
             networkRoomDialog.Show();
     }
 
     public void OnClickDedicate()
     {
+        PlayerSave.SetCharacter(SelectCharacter);
+        PlayerSave.SetHead(SelectHead);
+        PlayerSave.SetBomb(SelectBomb);
         var master = FindObjectOfType<MasterServerBehaviour>();
         master.StartServer();
     }
